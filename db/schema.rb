92993ec73e53e3cdbe6bc8c70f8d5a93fea889ff
Hunter Li <hunli.davis@gmail.com>
2018-09-14 09:30:18 -0700

    Adding password field to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_14_020344) do
+ActiveRecord::Schema.define(version: 2018_09_14_162553) do
 
   create_table "accounts", force: :cascade do |t|
     t.text "token"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_09_14_020344) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "account_id"
+    t.string "password"
     t.index ["account_id"], name: "index_users_on_account_id"
   end
 
