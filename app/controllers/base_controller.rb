303a04c407b3ffb10bf20b1d7622bbc3d8ae9d62
Hunter Li <hunli.davis@gmail.com>
2018-09-14 12:49:47 -0700

    Adding a login page

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -7,6 +7,6 @@ class BaseController < ApplicationController
   end
 
   def current_user
-    current_user = User.find_by_id(session[:current_user_id])
+    @current_user ||= User.find_by_id(session[:current_user_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'login/index'
   get 'user/index'
   get 'sign_up/new'
-  post 'sign_up/create'
   get 'welcome/index'
+
+  post 'login/signin'
+  post 'sign_up/create'
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
